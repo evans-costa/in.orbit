079168d7c6ad1d57b109edd22712809628cd14d1
Evandro Costa <evandro.souzac@gmail.com>
2024-09-10 20:51:52 -0300

    build: isetup db with `Drizzlee` ORM

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit';
+import { env } from './src/env';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './migrations',
+  dialect: 'postgresql',
+  migrations: {
+    prefix: 'supabase',
+  },
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+});

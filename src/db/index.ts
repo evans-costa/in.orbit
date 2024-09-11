ce771f4c2fe6ef1b3d4ce48230638d11da6dbe3f
Evandro Costa <evandro.souzac@gmail.com>
2024-09-10 23:44:29 -0300

    feat: add  file to connect to database

---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+import * as schema from './schema';
+import { env } from '../env';
+
+export const client = new Client({
+  connectionString: env.DATABASE_URL,
+});
+
+export const db = drizzle(client, { schema, logger: true });
